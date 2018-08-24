fee57caf454f98ffa49affa3d058c01fd3712dc7
Fedor Solovyev <the0@yandex-team.ru>
2018-08-24 18:51:31 +0300

    Fixed PATH for anaconda. Fixed colors. Fixed ssh command

--- bin/setup_new_host.sh
+++ bin/setup_new_host.sh
@@ -8,5 +8,5 @@ fi
 
 remote_host=$1
 
-ssh-copy-id "$remote_host"
+#ssh-copy-id "$remote_host"
 ssh -t "$remote_host" bash -c "\"curl -L 'https://raw.githubusercontent.com/sthe0/configs/master/install.sh' | bash\""
